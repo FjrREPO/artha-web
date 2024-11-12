f374c7cbcf1377eb7354705cabaf40edc32b79b2
FjrREPO <jossfajar21@gmail.com>
2024-11-12 18:49:40 +0700

    update route🌟

--- app/(client)/pool/[poolId]/page.tsx
+++ app/(client)/pool/[poolId]/page.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
+import React, { use } from 'react';
 import PoolIdPage from './_components/PoolIdPage';
 
-type Props = {
-    params: {
-        poolId: string
-    }
-}
+type Params = Promise<{ poolId: string }>
 
-const Page = ({ params }: Props) => {
+const Page = (props: { params: Params }) => {
+    const params = use(props.params);
+    const poolId = params.poolId;
+    
     return (
         <div className='pt-[100px]'>
-            <PoolIdPage PoolId={params.poolId} />
+            <PoolIdPage PoolId={poolId} />
         </div>
     );
 };
